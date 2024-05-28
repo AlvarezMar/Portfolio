ade5e52b48a657a921c3530db850500e506b0e6f
AlvarezMar <alvarez.majuan@hotmail.com>
2024-05-28 09:56:23 -0600

    Circular component & Arrow asset

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Navbar from './components/navbar/Navbar'
 import Projects from './views/projects/Projects'
 import About from './views/about/About'
 import Home from './views/home/Home'
+import CircularText from './components/circularText/CircularText'
 
 function App() {
 
   return <div>
     <Navbar/>
     <Home/>
+    <CircularText/>
     <About/>
     <Projects/>
   </div>
---
+++ src/components/circularText/CircularText.jsx
@@ -0,0 +1,35 @@
+import { useEffect } from 'react'
+import styles from './CircularText.module.css'
+import { useRef } from 'react'
+
+function CircularText(){
+    const divRef = useRef(null)
+
+    useEffect(() => {
+        // Verifica si la referencia divRef.current no es null
+        if (divRef.current) {
+          // Selecciona el elemento <p> dentro del div referenciado
+          const textElement = divRef.current.querySelector('p');
+          // Verifica si el elemento <p> no es null
+          if (textElement) {
+            const textContent = textElement.innerText; // Obtiene el texto del elemento <p>
+            // Divide el texto en caracteres individuales y envuelve cada uno en un <span>
+            textElement.innerHTML = textContent
+              .split('')
+              .map((char, i) => `<span style='transform:rotate(${i * 8.3}deg)'>${char}</span>`)
+              .join(''); // Junta todos los <span> en un solo string
+          }
+        }
+      }, []); 
+
+    return <div className={styles.circle}>
+        <div className={styles.inner}></div>
+        <div className={styles.text} ref={divRef}>
+            <p>Scroll down - Scroll down - </p>
+        </div>
+
+    </div>
+
+}
+
+export default CircularText
