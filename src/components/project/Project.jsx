117a61757c4d4943846beca7bfeb403ebb69aff0
AlvarezMar <alvarez.majuan@hotmail.com>
2024-08-20 19:33:54 -0600

    feat(components, views): Add responsiveness compatibility for more devices for Header and TechStack components. Add project images for Project view

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -4,7 +4,8 @@ import Tag from '../tag/Tag'
 import github from '../../assets/socials/github.svg'
 import link from '../../assets/elements/link.svg'
 
-function Project({title, description, technologies, imgURL}){
+function Project({title, description, technologies, imgURL, repoURL, websiteURL}){
+
     return <div className={`${styles.project} ${styles.container}`} data-aos="fade-left">
         <div className={styles.info}>
             <h2 className='my-3'>{title}</h2>
@@ -15,11 +16,11 @@ function Project({title, description, technologies, imgURL}){
             ))}
 
             <div className={styles.buttons}>
-                <a href="">
+                <a href={repoURL} target='_blank'>
                     <p>Code</p>
                     <img src={github} alt="" loading='lazy'/>
                 </a>
-                <a href="">
+                <a href={websiteURL} target='_blank' style={{pointerEvents: websiteURL ? 'auto' : 'none'}}>
                     <p>Preview</p>
                     <img src={link} alt="" loading='lazy'/>
                 </a>
--- src/views/projects/Projects.jsx
+++ src/views/projects/Projects.jsx
@@ -1,12 +1,12 @@
 import Project from '../../components/project/Project'
 import styles from './Projects.module.css'
-import img from '/images.png'
+// import img from '/images.png'
 import projects from '../../assets/sections/projects.svg'
 
 const portfolio = [
     {
         title: 'YouDrive',
-        description: 'This movie website allows user to explore movie information, add new movies through a form, and store information in a database.',
+        description: 'Collaborative application for managing vehicle rental information. This application allows users to post, modify vehicles, and manage rental processes.',
         technologies: [
             {
                 name: 'NestJS'
@@ -24,7 +24,9 @@ const portfolio = [
                 name: 'TypeScript'
             }
         ],
-        imgURL: img
+        imgURL: 'https://res.cloudinary.com/dwhmcn88z/image/upload/v1724203034/youDrive_dw21hi.png',
+        repoURL: 'https://github.com/maccari78/PM6-grupo3',
+        websiteURL: 'https://youdrive.vercel.app/'
     },
     {
         title: 'Doña Diabla',
@@ -46,7 +48,9 @@ const portfolio = [
                 name: 'Express'
             },
         ],
-        imgURL: img
+        imgURL: 'https://res.cloudinary.com/dwhmcn88z/image/upload/v1724203128/Do%C3%B1a_Diabla_v1_fkav1i.png',
+        repoURL: 'https://github.com/AlvarezMar/Dona-Diabla',
+        websiteURL: ''
     },
     {
         title: 'Movloom',
@@ -65,7 +69,9 @@ const portfolio = [
                 name: 'Mongoose'
             }
         ],
-        imgURL: img
+        imgURL: 'https://res.cloudinary.com/dwhmcn88z/image/upload/v1724203131/MovLoom_v1_xwayle.png',
+        repoURL: 'https://github.com/AlvarezMar/MovLoom',
+        websiteURL: ''
     }
     
 ]
@@ -76,11 +82,10 @@ function Projects(){
                 <img src={projects} alt="Projects section" width='30px'/>
                 <h2>Latest Projects</h2>
             </div>
-            <span>Under construction</span>
         </div>
         <div className={styles.portfolio}>
             {portfolio.map((project) => (
-                <Project key={project.title} title={project.title} description={project.description} technologies={project.technologies} imgURL={project.imgURL}/>
+                <Project key={project.title} title={project.title} description={project.description} technologies={project.technologies} imgURL={project.imgURL} repoURL={project.repoURL} websiteURL={project.websiteURL}/>
             ))}
         </div>
     </div>
